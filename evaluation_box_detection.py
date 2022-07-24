7b4eecbd6360f587f40885aeec46b56ecb417f00
Lucky-Aubrey Adam <76653560+Lucky-Aubrey@users.noreply.github.com>
2022-07-24 03:32:08 +0200

    Rename 2nd evaluation test

--- evaluation_box_detection.py
+++ evaluation_box_detection.py
@@ -1,61 +1,103 @@
+import glob
 
+file_list = glob.glob("data/sound_evaluation/images/*/*.png")
 
-#%% load data
-import tensorflow as tf
+# Load labels
+file_names = []
+class_list = []
+for file in file_list:
+    file_names.append( file.split('\\')[-1].split('.')[0])
+    class_list.append( file.split('\\')[-2])
 
-# Load voc2012 dataset
-from yolov3_tf2.dataset import transform_images, load_tfrecord_dataset
 import numpy as np
+folder = 'data/sound_evaluation/corrected_labels/'
+npy_list = []
+except_list = []
+for i, (file, true_class) in enumerate(zip(file_names, class_list)):
+    if true_class != 'no_sound':
+        try:
+            npy_list.append(np.load(folder + file + '.npy'))
+        except:
+            except_list.append(file_list[i])
+                
+    else:
+        npy_list.append(np.array([[-1.,0.,0.,0.,0.]]))
+        
+npy_list = np.array(npy_list, dtype=object)
 
-tfrecord = './data/sound_val.tfrecord'
-classes = './data/sound.names'
-size = 416
-anchors = np.array([(10, 13), (16, 30), (33, 23), (30, 61), (62, 45),
-                         (59, 119), (116, 90), (156, 198), (373, 326)],
-                        np.float32) / 416
-anchor_masks = np.array([[6, 7, 8], [3, 4, 5], [0, 1, 2]])
+# Images
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-name_dict = {
-    0: 'artefact',
-    1: 'click',
-    2: 'squeal',
-    3: 'wirebrush'
-    }
+images = tf.data.Dataset.from_tensor_slices(
+    [plt.imread(a)[...,:3] for a in file_list]
+    )
 
-table = tf.lookup.StaticHashTable(
-    initializer=tf.lookup.KeyValueTensorInitializer(
-        keys=tf.constant(['artefact', 'click', 'squeal', 'wirebrush']),
-        values=tf.constant([0, 1, 2, 3]),
-    ),
-    default_value=tf.constant(-1),
-    name="class_weight"
-)
+def resize_img(image):
+    return tf.image.resize(image, [416,416])
 
-# dataset = ds.load_tfrecord_dataset(tfrecord, classes, size)
+images = images.map(resize_img)
 
-dataset = tf.data.Dataset.list_files('data/sound_evaluation/images/*/*.png',
-                                     shuffle = False)
-from yolo_utils import process_image_for_detection, scale
+#%% Test npy
 
-# Loading dataset
-
-val_dataset = dataset.map(process_image_for_detection).map(scale)
 
 #%%
 
-bs = 2 # Batch size: works only for bs > 1 # TODO implement for bs = 1
-ns = 32 # Number of images
+import config
+
+file_name = 'yolov3_2'
+img_size = config.IMG_SIZE # Should be multiple of 32
+grid_list = config.GRID_LIST # Basically this is img_size / 32
+num_classes = config.NUM_CLASSES
+weights = config.WEIGHTS
+anchors = config.ANCHORS
+epochs=config.EPOCHS
+batch_size=config.BATCH_SIZE
+learning_rate = config.LEARNING_RATE # TODO
+
+#%% Model
+from model import build_model
+# from weight_converter_model import build_model
+
+# # VOC2012
+# num_classes = 20
+# Sound
+num_classes = 4
+#
+yolo = build_model(num_classes)
+
+# yolo.load_weights('weights/yolov3_transferlearning_1.h5')
+# yolo.load_weights('weights/yolov3_voc2012.h5')
+# yolo.load_weights('checkpoints/yolov3_voc2012_github_loss5.tf')
+# yolo.load_weights('checkpoints/yolov3_voc2012_github_loss2.tf')
+# yolo.load_weights('checkpoints/yolov3_voc2012_my_loss_pre3.tf')
+# yolo.load_weights('checkpoints/yolov3_sound_my_loss4.tf')
+yolo.load_weights('checkpoints/yolov3_sound_from_scratch16.tf')
+
+#%%
+
+bs = 16 # Batch size: works only for bs > 1 # TODO implement for bs = 1
 anchors = config.ANCHORS
 
-prediction_table = []
+evaluation_table = {0: {'TP':0, 'FP':0, 'FN':0},
+                    1: {'TP':0, 'FP':0, 'FN':0},
+                    2: {'TP':0, 'FP':0, 'FN':0},
+                    3: {'TP':0, 'FP':0, 'FN':0}}
 import numpy as np
 import time
+from yolo_utils import intersectionOverUnion
 start = time.time()
 img_i = 0
-for x, y in val_dataset.batch(bs).take(10):
-    output = yolo(x)
+for i, img in enumerate(images.batch(bs)):
+    # Prediction
+    output = yolo(img)
+    # True labels
+    target = npy_list[i*bs:(i+1)*bs]
+    
     #
     from yolo_utils import yoloBoxes, _nms
+    
+    # [BOXES/CONF/CLASS][BS][GRID_Y][GRID_X][ANCHORS][...]
     y1 = yoloBoxes(output[0], anchors[0], num_classes)
     y2 = yoloBoxes(output[1], anchors[1], num_classes)
     y3 = yoloBoxes(output[2], anchors[2], num_classes)
@@ -71,79 +113,75 @@ for x, y in val_dataset.batch(bs).take(10):
          soft_nms_sigma=0.)
         
     for bs_i, res in enumerate(result):
+        # get predictions and true values
+        # [[x1,y1,x2,y2],...],scores,[class,...],....
         boxes, scores, classes, valid_detections = res
-        vd = valid_detections[0]
-        if vd == 0:
-            unique_classes = []
-        else:
-            unique_classes, _ = tf.unique(
-                tf.squeeze(
-                    tf.stack(classes[:vd],0),0))
-    
-        compare_table = {
-            0: ['F','F'],
-            1: ['F','F'],
-            2: ['F','F'],
-            3: ['F','F'],
-            }
-        true_class = int(table.lookup(y[bs_i]))
-        if true_class != -1:
-            compare_table[true_class][1] = 'T'
-        
-        for c in unique_classes:
-            pred_class = int(c)
-            compare_table[pred_class][0] = 'T'
+        # [class, x1, y1, x2, y2]
+        labels = target[bs_i]
+        # list boxes for each class separate in dictionary
+        pred_boxes = {0: [],
+                      1: [],
+                      2: [],
+                      3: []}
+        true_boxes = {0: [],
+                      1: [],
+                      2: [],
+                      3: []}
+        # put boxes inside dictionaries
+        for b, c in zip(boxes[0],classes[0]):
+            pred_boxes[int(c)].append(b)
+        # print('\nLabels:') # TODO delete
+        for label in labels:
+            if label[0] != -1:
+                true_boxes[label[0]].append(label[1:5])
+        #     print(f'class: {label[0]}')
+        #     print(f'box: {label[1:5]}')
+        # print('')
             
-        prediction_table.append(compare_table)
-        img_i+=1
-    
-
-check0 = time.time()
-print(f'Prediction of {ns} images using batch size of {bs}: {check0-start}s')
-# print(f'\nAmount images: {len(prediction_table)}')
-# for i, a in enumerate(prediction_table):
-#     print(i+1, a)
-    
-#%% Evaluate Table
-
-res_table = {
-    'squeal': {'TP': 0, 'FP': 0, 'FN': 0, 'TN': 0},
-    'wirebrush': {'TP': 0, 'FP': 0, 'FN': 0, 'TN': 0},
-    'click': {'TP': 0, 'FP': 0, 'FN': 0, 'TN': 0},
-    'artefact': {'TP': 0, 'FP': 0, 'FN': 0, 'TN': 0}
+        for class_key in pred_boxes:
+            j = 0
+            while j < len(pred_boxes[class_key]):
+                pred_box = pred_boxes[class_key][j]
+                k = 0 # TODO 
+                if len(true_boxes[class_key]) == 0: 
+                    j += 1
+                    continue
+                while k < len(true_boxes[class_key]):
+                    true_box = true_boxes[class_key][k]
+                    if intersectionOverUnion(true_box, pred_box) > 0.5:
+                        true_boxes[class_key].pop(k)
+                        pred_boxes[class_key].pop(j)
+                        evaluation_table[class_key]['TP'] += 1
+                    else:
+                        k += 1
+                j += 1
+            evaluation_table[class_key]['FP'] += len(pred_boxes[class_key])
+            evaluation_table[class_key]['FN'] += len(true_boxes[class_key])
+            
+name_dict = {
+    0: 'artefact',
+    1: 'click',
+    2: 'squeal',
+    3: 'wirebrush'
     }
 
-for pred in prediction_table:
-    for key in pred:
-        comparison = pred[key][0]+pred[key][1]
-        table_key = name_dict[key]
-        if comparison == 'TT':
-            res_table[table_key]['TP'] +=1
-        elif comparison == 'TF':
-            res_table[table_key]['FP'] +=1
-        elif comparison == 'FT':
-            res_table[table_key]['FN'] +=1
-        elif comparison == 'FF':
-            res_table[table_key]['TN'] +=1
-            
+check0 = time.time()
+print(f'Prediction of {len(images)} images using batch size of {bs}: {check0-start}s')
+for e in evaluation_table:
+    print(name_dict[e], evaluation_table[e])
+#%% Evaluate Table
 
-for key in res_table:
-    TP = res_table[key]['TP']
-    FP = res_table[key]['FP']
-    FN = res_table[key]['FN']
-    TN = res_table[key]['TN']
+for key in evaluation_table:
+    TP = evaluation_table[key]['TP']
+    FP = evaluation_table[key]['FP']
+    FN = evaluation_table[key]['FN']
     recall =  TP / (TP+FN)
     precision = TP / (TP+FP)
     f1 = 2 * recall * precision / (recall + precision)
-    res_table[key]['recall'] = round(recall,2)
-    res_table[key]['precision'] = round(precision,2)
-    res_table[key]['f1'] =  round(f1,2)
+    evaluation_table[key]['recall'] = round(recall,2)
+    evaluation_table[key]['precision'] = round(precision,2)
+    evaluation_table[key]['f1'] =  round(f1,2)
     
-for key in res_table:
-    print(key,':', res_table[key])
+for key in evaluation_table:
+    print(name_dict[key],':', evaluation_table[key])
 
-# #%% Save as json file
-# import json
-# file_name = 'yolov3_sound_from_scratch16_iou0_5_score0_5'
-# with open(f'evaluation/{file_name}.json','w+') as f:
-#     json.dump(res_table, f, indent=4)
